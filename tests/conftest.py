fa694901bfdb4c5c727e3b8fc96dab04038b3e47
Jakub Szafrański <s@samu.pl>
2023-03-20 11:41:24 +0100

    feat: token based authorization

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 """Global configuration for all tests."""
+from typing import Generator
+
 import pytest
 import responses
 
 
 @pytest.fixture()
-def mocked_responses() -> responses.RequestsMock:
+def mocked_responses() -> Generator[responses.RequestsMock, None, None]:
     """Pytest-compatible fixture for responses."""
     with responses.RequestsMock() as rsps:
         yield rsps

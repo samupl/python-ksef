5ec59bc607bd3392bb821ce2de729f0dc2b50f7a
Jakub Szafrański <s@samu.pl>
2023-12-13 13:43:15 +0100

    Initialize authorization (#17)

---
+++ src/ksef/client.py
@@ -0,0 +1,43 @@
+"""Base client for interacting with the KSEF API."""
+from typing import Dict, Mapping, Optional, Union, cast
+from urllib.parse import urlencode, urljoin
+
+import requests
+from requests import Request
+
+from ksef.auth.base import Authorization
+from ksef.constants import BASE_URL, URL_QUERY_INVOICES
+
+
+class Client:
+    """Base client for interacting with the KSEF API."""
+
+    def __init__(self, authorization: Authorization, base_url: str = BASE_URL):
+        self.authorization = authorization
+        self.base_url = base_url
+        self.session = requests.Session()
+
+    def build_url(self, url: str, params: Optional[Mapping[str, Union[str, int]]] = None) -> str:
+        """Construct a full URL."""
+        url = urljoin(base=self.base_url, url=url)
+        if params is not None:
+            param_str = urlencode(params)
+            return f"{url}?{param_str}"
+
+        return url
+
+    def search_invoices(self, page_size: int = 100, page_offset: int = 0) -> Dict[str, str]:
+        """Search for invoices with the specified page size and offset."""
+        params = {
+            "PageSize": page_size,
+            "PageOffset": page_offset,
+        }
+        request = Request(
+            method="POST",
+            url=self.build_url(url=URL_QUERY_INVOICES, params=params),
+        )
+        request = self.authorization.modify_request(request)
+        prepared_request = request.prepare()
+        response = self.session.send(prepared_request)
+        data = cast(Dict[str, str], response.json())
+        return data
